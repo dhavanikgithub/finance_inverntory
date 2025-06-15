a1ee207acd9e5c3b6102982deefcf61c57e1c0e3
Dhavanik <kanzariyagovindbhai21@gnu.ac.in>
2025-06-15 09:15:59 +0530

    refactor: improve remark textarea styling for better user experience

--- src/components/TransactionModal.tsx
+++ src/components/TransactionModal.tsx
@@ -278,7 +278,7 @@ const TransactionModal = ({
                     <label htmlFor="remark" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Remark</label>
                     <textarea
                         rows={4}
-                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white ring-0 focus:ring-0 focus:outline-none"
                         placeholder="Write your remark here..."
                         value={formData.remark || ''}
                         onInput={(e) => setFormData((prevState) => ({ ...prevState, remark: (e.target as HTMLInputElement).value }))}
